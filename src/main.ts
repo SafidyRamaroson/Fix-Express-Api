2bacb44b46fcca3a1ec6e57a30515a5bb367d0b8
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2025-08-16 21:31:19 +0300

    feat: Prefix all API routes with /api/v1

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Prefix all API route with /api/v1
+  app.setGlobalPrefix('/api/v1');
+
   await app.listen(3001);
 }
 bootstrap();
